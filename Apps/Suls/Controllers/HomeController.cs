9987f446068394851d4b32813396d9a11c278651
IvoKirilov11 <mefistofel_1980@abv.bg>
2022-01-31 15:07:11 +0200

    Suls

---
+++ Apps/Suls/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Suls.Services;
+using SUS.MvcFramework;
+using SUSHTTP;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Suls.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IProblemsService problemsService;
+
+        public HomeController(IProblemsService problemsService)
+        {
+            this.problemsService = problemsService;
+        }
+
+
+        [HttpGet("/")]
+        public HttpResponse Index()
+        {
+            if (this.IsUserSignedIn())
+            {
+                var viewModel = this.problemsService.GetAll();
+                return this.View(viewModel, "IndexLoggedIn");
+            }
+            else
+            {
+                return this.View();
+            }
+        }
+    }
+}
